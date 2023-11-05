94da67bec691efa5faf6623ac54d57b60e0005f0
Nicolas Bueno <nicolasbrian202@gmail.com>
2023-11-05 09:27:21 -0300

    entities cliente,reserva

---
+++ src/main/java/com/senac/mesaBooking/model/Reserva.java
@@ -0,0 +1,31 @@
+package com.senac.mesaBooking.model;
+
+import java.sql.Date;
+import java.sql.Time;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class Reserva {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date data;
+    private Time hora;
+    private Integer numeroPessoas;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurante_id")
+    private Restaurante restaurante;
+}
